259f6cc778dfd6e245509b66432f5ab34b16c43e
Fernando <ferdelamad@gmail.com>
2018-12-04 14:37:14 -0800

    Added the initial bioler plate pages

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "705d611d31f49a3021dd";
+/******/ 	var hotCurrentHash = "e0d15359b10ae0d2cf54";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
